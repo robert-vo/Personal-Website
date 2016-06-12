eb4a2df98d033939b291494b6b625d184c9140e9
robert-vo <robertvo79@gmail.com>
2016-06-11 19:01:26 -0500

    Home layout should be completed. Made it where it'll read from a JSON file and print out the appropriate buttons

--- airbnb/app.js
+++ airbnb/app.js
@@ -1,1 +1,9 @@
-angular.module('tripPlannerApp', []);
+var App = angular.module('tripPlannerApp', []);
+
+App.controller('tripPlannerController', function($scope, $http) {
+    $http.get('../resources/json/cities.json')
+        .then(function(result){
+            $scope.cities = result.data;
+        });
+    
+});
