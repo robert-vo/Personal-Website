5b9851c793cdebd5fec1e5f804836ec2690677e5
robert-vo <robertvo79@gmail.com>
2016-06-11 19:41:01 -0500

    Testing stuff out with the width of the browser

--- airbnb/app.js
+++ airbnb/app.js
@@ -7,3 +7,39 @@ App.controller('tripPlannerController', function($scope, $http) {
         });
     
 });
+
+App.directive('resize', function ($window) {
+    return function (scope, element) {
+        var w = angular.element($window);
+        scope.getWindowDimensions = function () {
+            return {
+                'h': w.height(),
+                'w': w.width()
+            };
+        };
+        scope.$watch(scope.getWindowDimensions, function (newValue, oldValue) {
+            scope.windowHeight = newValue.h;
+            scope.windowWidth = newValue.w;
+
+            scope.style = function () {
+                if((newValue.w - 100) < 700) {
+                    return {
+                        'height': 'small af',
+                        'width': (newValue.w - 100) + 'px'
+                    };
+                }
+                else {
+                    return {
+                        'height': (newValue.h - 12200) + 'px',
+                        'width': (newValue.w - 100) + 'px'
+                    };
+                }
+            };
+
+        }, true);
+
+        w.bind('resize', function () {
+            scope.$apply();
+        });
+    }
+})
